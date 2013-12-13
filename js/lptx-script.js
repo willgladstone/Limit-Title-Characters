e0d919c76ad140b3f05fa69ffcce8e0aa14e91b5
willgladstone <will@alleyinteractive.com>
2013-12-13 13:09:33 -0500

    JS Fix to run only on post & post edit pages

--- js/lptx-script.js
+++ js/lptx-script.js
@@ -1,28 +1,30 @@
 jQuery(document).ready(function(){
-	var i;
-	var maximum = jQuery('#lptx_maximum').val();
-	i = jQuery('#title').val().length;
-    jQuery('#title').keyup(function(){
-		checkIfMaximum(this);
-	});
-	function checkIfMaximum(elemId){
-		var counter = jQuery(elemId).val().length;
-		i = jQuery(elemId).val().length;
-		if(counter > maximum){
-			jQuery('#lptx-counter').addClass('lptx-over');
-		}else{
-			jQuery('#lptx-counter').removeClass('lptx-over');
+	if ( window.location.pathname == '/wp-admin/post-new.php' || window.location.pathname == '/wp-admin/post.php' ) {
+		var i;
+		var maximum = jQuery('#lptx_maximum').val();
+		i = jQuery('#title').val().length;
+	    jQuery('#title').keyup(function(){
+			checkIfMaximum(this);
+		});
+		function checkIfMaximum(elemId){
+			var counter = jQuery(elemId).val().length;
+			i = jQuery(elemId).val().length;
+			if(counter > maximum){
+				jQuery('#lptx-counter').addClass('lptx-over');
+			}else{
+				jQuery('#lptx-counter').removeClass('lptx-over');
+			}
+			jQuery('#lptx-counter').html(counter);
 		}
-		jQuery('#lptx-counter').html(counter);
+		jQuery('#empty-title').click(function(){jQuery('#title').val("").focus();jQuery('#lptx-counter').html(0);});
+		jQuery('#publish').mousedown(function(){
+			if(i <= maximum){
+				//if we're not over the maximum allowed by the plugin, everything is fine. But you could do something here if you wanted.
+				return true;
+			}else{
+				alert(traductionFromWP.alertMessage);
+				return false;
+			}
+		});
 	}
-	jQuery('#empty-title').click(function(){jQuery('#title').val("").focus();jQuery('#lptx-counter').html(0);});
-	jQuery('#publish').mousedown(function(){
-		if(i <= maximum){
-			//if we're not over the maximum allowed by the plugin, everything is fine. But you could do something here if you wanted.
-			return true;
-		}else{
-			alert(traductionFromWP.alertMessage);
-			return false;
-		}
-	});
 });
